d16171eca30002a852aacba6209d534fdc11056f
Imran Shakil <imranippu@yahoo.com>
2018-02-26 11:27:49 +0900

    company url

---
+++ Controller/Adminhtml/Index/Veriteworks.php
@@ -0,0 +1,32 @@
+<?php
+namespace Veriteworks\Base\Controller\Adminhtml\Index;
+
+use Magento\Backend\App\Action;
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\Controller\Result\Redirect;
+use Magento\Framework\Controller\ResultFactory;
+
+class Veriteworks extends \Magento\Backend\App\Action
+{
+
+    /**
+     * Veriteworks constructor.
+     * @param \Magento\Backend\App\Action\Context $context
+     */
+    public function __construct(
+        Context $context
+    )
+    {
+        parent::__construct($context);
+    }
+
+    /**
+     * @return \Magento\Backend\Model\View\Result\Page|\Magento\Framework\App\ResponseInterface|\Magento\Framework\Controller\ResultInterface
+     */
+    public function execute()
+    {
+        $resultRedirect = $this->resultFactory->create(ResultFactory::TYPE_REDIRECT);
+        $resultRedirect->setUrl('https://veriteworks.co.jp/');
+        return $resultRedirect;
+    }
+}
